e100fee4ab93c291f19a4570f007bc60d2bd0de5
Sumit Pal <asr.sumitpal92@gmail.com>
2022-12-24 20:54:40 -0500

    Added Class with helper methods to handel text files and save data to text files.
    Finished the text connector.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,15 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModel.csv";
         // TODO - Wire up the CreatePrize for the text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // Load the text file
+            // Convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find the max ID
+            int currentId = 1;
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id = currentId;
+
+            // Add the new record with the new ID (max + 1)
+            prizes.Add(model);
+
+
+            // Convert the prizes to list<string>
+            // Save the list<string> to the file.
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
