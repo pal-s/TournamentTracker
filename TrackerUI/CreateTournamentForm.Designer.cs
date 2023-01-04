f11e6d7e81584f7a189e8f62380338a5efb76dfc
Sumit Pal <asr.sumitpal92@gmail.com>
2023-01-04 03:13:51 -0500

    -Fix label text in Create Tournament form

--- TrackerUI/CreateTournamentForm.Designer.cs
+++ TrackerUI/CreateTournamentForm.Designer.cs
@@ -209,9 +209,9 @@
             this.prizesLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             this.prizesLabel.Location = new System.Drawing.Point(609, 380);
             this.prizesLabel.Name = "prizesLabel";
-            this.prizesLabel.Size = new System.Drawing.Size(198, 37);
+            this.prizesLabel.Size = new System.Drawing.Size(85, 37);
             this.prizesLabel.TabIndex = 22;
-            this.prizesLabel.Text = "Teams / Players";
+            this.prizesLabel.Text = "Prizes";
             // 
             // prizesListBox
             // 
